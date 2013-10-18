a430720f6f8f52511a736aa9a4acc080e4a8dcc8
FrostDuke <shaun.jackson@gryphontranslations.com>
2013-10-18 12:02:52 +0200

    Finishing layout

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  subject { page }
+   
+   describe "Home Page" do
+     before {visit root_path}
+     
+     it { should have_content('Sample App') }
+     it { should have_title(full_title('')) }
+     it { should_not have_title('| Home')} 
+  end       
+      
+   describe "Help Page" do
+     before {visit help_path}
+     
+     it {should have_content('Help')}
+     it {should have_title(full_title('Help'))}
+     
+   end
+   
+   describe "About Page" do
+     before {visit about_path}
+     
+     it {should have_content('About')}
+     it {should have_title(full_title('About'))}
+   
+  end
+end
